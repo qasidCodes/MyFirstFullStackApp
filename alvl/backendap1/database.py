d2599e3a5bbff20fa9ceb38229e0da6eb810d61e
qasidCodes <132705929+qasidCodes@users.noreply.github.com>
2024-04-22 22:31:08 +0100

    idk

--- alvl/backendap1/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-URL_DATABASE = 'mysql+pymysql://root:Qwertyops99@localhost:3306/ApprenticeDatabase'
-engine = create_engine(URL_DATABASE)
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-base = declarative_base()
